6deeba87a0ba59de5e21e20186f313423c427d08
katya-test <katyasuhr@gmail.com>
2024-08-26 22:06:01 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const el = document.createElement('footer');
-const connect = document.querySelector('#connect');
-connect.append(el);
+const messages = document.querySelector('#messages');
+messages.append(el);
 
 const today = new Date();
 const thisYear = today.getFullYear();
@@ -27,3 +27,67 @@ for(let i = 0; i < skills.length; i++) {
     skillList.appendChild(skill);
     skill.style.margin = '0 3em';
 }
+
+// Creating the Submit Form
+const messageForm = document.querySelector('[name="leave_message"]');
+const leaveMessage = document.querySelector('#leave_message');
+
+messageForm.addEventListener('submit', submitMessage);
+
+function submitMessage(event) {
+    event.preventDefault(); // Prevent the form from submitting the default way
+    
+    const username = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+    
+    console.log(username, email, message);
+
+    const messageSection = document.querySelector('#messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li'); //makes a new list item
+    messageList.appendChild(newMessage);
+
+    newMessage.innerHTML = `
+    <a href="mailto:${email}">${username}</a>
+    <span> ${message}</span>`;
+
+    // Remove button creation
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML = "Remove";
+    removeButton.setAttribute("type", "button");
+    removeButton.setAttribute("class", "remove_button");
+
+    removeButton.addEventListener('click', function(){
+        const entry = removeButton.parentNode;
+        entry.removeChild(removeButton);
+        newMessage.remove();
+
+    // To hide the message section when empty
+        if(messageList.children.length === 0) {
+            messageSection.style.display = 'none';
+            leaveMessage.append(el);
+        }
+    });  
+   
+//   Creating the Edit button
+    const editButton = document.createElement('button');
+    editButton.innerHTML = "Edit";
+    editButton.setAttribute("type", "button");
+    editButton.setAttribute("class", "edit_button");
+
+    editButton.addEventListener('click', function(){
+       newMessage.innerHTML = `<a href="mailto:${email}">${username}</a>
+       <textarea> ${message}</textarea>`
+       newMessage.appendChild(removeButton);
+       newMessage.appendChild(editButton); 
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    newMessage.appendChild(editButton);   
+
+    messageForm.reset(); //to reset the fields after submit
+}
+
+
